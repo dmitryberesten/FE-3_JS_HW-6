0873d97c12cbb2359cf5d22c2ebda1f337919efc
Dmytrii Beresten <87872240+dmitryberesten@users.noreply.github.com>
2024-12-09 14:41:38 +0200

    FE-3_JS_HW-6

---
+++ task-4.js
@@ -0,0 +1,10 @@
+// Створити масив з числами від 1 до 5.
+// Вивести на екран кожен елемент масиву за допомогою циклу while.
+
+const numbers = [1, 2, 3, 4, 5];
+let i = 0;
+
+while (i < numbers.length) {
+  console.log(numbers[i]);
+  i++;
+}
